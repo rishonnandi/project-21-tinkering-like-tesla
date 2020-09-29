c2a4cefd8c98d41c422ccb36b6383a100df291f7
rishonnandi <67328755+rishonnandi@users.noreply.github.com>
2020-09-29 23:00:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var wall,thickness;
+var bullet,speed,weight;
+function setup() {
+  createCanvas(1600,400);
+  thickness=random(22,83);
+  speed =random(223,321);
+  weight=random(30,52);
+   wall=createSprite(1200,300,thickness,height/2);
+   bullet = createSprite(50, 300, 100, 50);
+   bullet.velocityX=speed;
+   wall.shapeColor=color(80,80,80);
+}
+
+function draw() {
+  background(0,0,0); 
+  
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage=weight*speed*speed/(thickness*thickness*thickness);
+  }
+  if(damage>10){
+   wall.shapeColor=color(255,0,0); 
+  }
+  if(damage<10){
+    wall.shapeColor=color(0,255,0);
+  
+  }
+  
+
+
+
+
+
+
+  drawSprites();
+}
+function hasCollided(lbullet,lwall) {
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallleftEdge=lwall.x;
+  if(bulletRightEdge>=wallleftEdge){
+    return true
+  }
+  return false;
+
+}
